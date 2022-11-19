c6df874a597126b4f2e4cd7ad12a16a142b81229
Rhahi <git@rhahi.com>
2022-11-19 15:33:51 +0100

    update tests

--- test/logger_client.jl
+++ test/logger_client.jl
@@ -3,4 +3,21 @@ using RemoteLogging.Terminal
 logging, progress = activate()
 
 @remotelog 1000 "hello"
+
+id = progress_init("Test")
+sleep(1)
+progress_update(id, 0.5)
+sleep(1)
+progress_update(id, 0.9, "Test name")
+sleep(1)
+progress_end(id, name="ending")
+id2 = progress_init("Test2")
+progress_update(id2, 0.9)
+sleep(1)
+progress_update(id2, 1.1)
+
 @log_attention "test"
+
+progress_init("Unfinished 1")
+id3 = progress_init("Unfinished 2")
+id3 = progress_update(id3, 0.5)
--- test/logger_server.jl
+++ test/logger_server.jl
@@ -1,8 +1,3 @@
 using RemoteLogging
 
-chan1 = Channel{LogMessage}(100)
-chan2 = Channel{ProgressMessage}(100)
-server1 = host_logger(chan1)
-server2 = host_progress(chan2)
-begin_logging_sink(chan1)
-begin_progress_sink(chan2)
+active = activate_terminal()
