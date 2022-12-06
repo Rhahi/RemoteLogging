3519f80292a6f0377fe5a0eb3244e0c70846078f
Rhahi <git@rhahi.com>
2022-12-07 00:57:03 +0100

    fix: add progress subinit for child progressbars

--- src/remotelogger.jl
+++ src/remotelogger.jl
@@ -77,8 +77,12 @@ function progress_init(parentid::UUID, name::String)
 end
 progress_init(parentid::ProgressId, name::String) = progress_init(parentid.id, name)
 progress_init(name::String="") = progress_init(ProgressLogging.ROOTID, name)
+progress_init(parentid::Nothing, name::String) = progress_init(ProgressLogging.ROOTID, name)
 progress_init(parentid, name::Nothing) = nothing
 progress_init(name::Nothing) = nothing
+progress_subinit(parentid::Nothing, name::String) = nothing
+progress_subinit(parentid::Union{ProgressId, UUID}, name::String) = progress_init(parentid, "↱"*name)
+progress_subinit(parentid::Union{ProgressId, UUID}, name::Nothing) = progress_init(parentid, "↱")
 
 """Update progress bar. Implicitly end when reaching 1."""
 function progress_update(id::ProgressId, fraction, name=nothing)
